fffae3cfcb5f94d9668e1798681b55573946d36a
Gustavo Rivero <tavomak@gmail.com>
2023-05-21 12:27:31 -0400

    App commented

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 import TagManager from 'react-gtm-module';
-import Loading from 'components/Atoms/Loading';
+// import Loading from 'components/Atoms/Loading';
 import 'react-toastify/dist/ReactToastify.css';
 import 'styles/main.scss';
 
@@ -11,27 +11,27 @@ const tagManagerArgs = {
 };
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const [loading, setLoading] = useState(false);
+  // const router = useRouter();
+  // const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     TagManager.initialize(tagManagerArgs);
   }, []);
 
-  useEffect(() => {
-    const handleStart = (url: string) => (url !== router.asPath) && setLoading(true);
-    const handleComplete = (url: string) => (url === router.asPath) && setLoading(false);
+  // useEffect(() => {
+  //   const handleStart = (url: string) => (url !== router.asPath) && setLoading(true);
+  //   const handleComplete = (url: string) => (url === router.asPath) && setLoading(false);
 
-    router.events.on('routeChangeStart', handleStart);
-    router.events.on('routeChangeComplete', handleComplete);
-    router.events.on('routeChangeError', handleComplete);
+  //   router.events.on('routeChangeStart', handleStart);
+  //   router.events.on('routeChangeComplete', handleComplete);
+  //   router.events.on('routeChangeError', handleComplete);
 
-    return () => {
-      router.events.off('routeChangeStart', handleStart);
-      router.events.off('routeChangeComplete', handleComplete);
-      router.events.off('routeChangeError', handleComplete);
-    };
-  }, [router]);
+  //   return () => {
+  //     router.events.off('routeChangeStart', handleStart);
+  //     router.events.off('routeChangeComplete', handleComplete);
+  //     router.events.off('routeChangeError', handleComplete);
+  //   };
+  // }, [router]);
 
   // if (loading) return <Loading />;
   return <Component {...pageProps} />;
